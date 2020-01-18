f15daac82c8b29209a6343a7e31a9fb0486b4918
David Whitlock <david.m.whitlock@gmail.com>
2020-01-17 20:32:06 -0800

    wagon-git commit

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"advanced"},{"l":"All Packages","url":"allpackages-index.html"},{"l":"beginner"},{"l":"edu.pdx.cs410J.whitlock"},{"l":"intermediate"},{"l":"java7"},{"l":"java8"}]
